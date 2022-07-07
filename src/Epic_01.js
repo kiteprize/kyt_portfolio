0f5c774ccd5bb2c9d6b5b534a2a85a4b2991e5e3
kiteprize <dustkd4294@naver.com>
2022-07-07 22:31:33 +0900

    Epic 1 end

---
+++ src/Epic_01.js
@@ -0,0 +1,64 @@
+import React, {Component, useEffect } from 'react';
+import ReactTextTransition from "react-text-transition";
+import {BrowserView, MobileView} from 'react-device-detect'
+import './Epic_01.css';
+
+const texts = ["제네럴리스트가 되려하는","성장을 중요시하는","트렌트를 배우고 있는"];
+
+class IntroduceLine extends Component{
+    state = {
+        textIndex: 0,
+        textFastIndex: 0,
+        paragraphIndex: 0,
+        customIndex: 0
+    };
+    
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({
+            textFastIndex: this.state.textFastIndex + 1
+            });
+        }, 3000);
+    }    
+
+    render(){
+        return(
+            <React.Fragment>
+                <ReactTextTransition direction="up" style={{justifyContent : "center", alignItems : "center"}} inline>
+                    {texts[this.state.textFastIndex % texts.length]}
+                </ReactTextTransition>
+            </React.Fragment>
+        );
+    }
+}
+
+function App() {
+  const setScreenSize = () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+  useEffect(() => {
+    setScreenSize();
+    window.addEventListener("resize", setScreenSize);
+  });
+  return (
+    <div className="App">
+      <BrowserView>
+        <div className="App-header">
+          <IntroduceLine/>
+            <div className="Underline"></div>
+          디자이너 김용태 입니다.
+        </div>
+      </BrowserView>
+      <MobileView>
+        <div className="mobile-App-header">
+          <IntroduceLine/>
+            <div className="Underline"></div>
+          디자이너 김용태 입니다.
+        </div>
+      </MobileView>
+    </div>
+  );
+}
+
+export default App;
